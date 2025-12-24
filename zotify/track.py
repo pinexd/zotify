ac089b2b9eef48e6a58ba827feee99615c0193f3
pinexd <105119099+pinexd@users.noreply.github.com>
2025-12-24 02:44:48 -0600

    tokenmanager

--- zotify/track.py
+++ zotify/track.py
@@ -48,10 +48,7 @@ def get_followed_artists() -> list:
 
 from zotify.tokenmanager import SpotifyTokenManager
 
-token_manager = SpotifyTokenManager(
-    client_id="",
-    client_secret=""
-)
+token_manager = SpotifyTokenManager()
 
 def get_song_info(song_id) -> Tuple[List[str], List[Any], str, str, Any, Any, Any, Any, Any, Any, int]:
     """ Retrieves metadata for downloaded songs using own Spotify API credentials """
